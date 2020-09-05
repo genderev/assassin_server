9c9a9e12705f8fa0fe037ea802660b6021039cb6
v <>
2020-09-05 14:04:58 -0400

    .

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send(JSON.stringify('Thanks': 'Visit gitter.im/assassindb for help!'))
+  res.send(JSON.stringify({'Thanks': 'Visit gitter.im/assassindb for help!'}))
 })
 app.post('/', function (req, res) {
   res.send('POST request to the homepage')
 })
+
+app.listen(8080)
